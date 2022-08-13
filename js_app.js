d8205026103317ce14c1540625f7e339934b78b3
moneerzaki <moneerzaki@aucegypt.edu>
2022-08-14 01:13:28 +0200

    1st js trial

--- js_app.js
+++ js_app.js
@@ -1,5 +1,7 @@
-const section = document.querySelector('.section');
-section[0].onclick = function(){
-    section[0].style.back
-};
-// section.style.backgroundcolor = "black";
+
+
+var section2 = document.querySelector('#section2');
+section2.classList.add('section_even');
+
+var section4 = document.querySelector('#section4');
+section4.classList.add('section_even');
