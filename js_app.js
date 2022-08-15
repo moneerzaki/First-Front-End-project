586b019e6a3f13141fb4343bcd087e2213717b81
moneerzaki <moneerzaki@aucegypt.edu>
2022-08-15 15:42:44 +0200

    project is over

--- js_app.js
+++ js_app.js
@@ -14,19 +14,28 @@ var lorem1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ill
 
 
 
+
 // creating a new section 3 in the nav bar. 
-var section3NavLink = document.createElement('a');      // a
-section3NavLink.setAttribute('href', "#section3");
-section3NavLink.textContent = "Sect 3";
-
-var section3Nav = document.createElement('li');         // li
-// section3Nav.setAttribute('id',"#nav-item3") ;
-section3Nav.appendChild(section3NavLink);
-
-var navitems = document.querySelector('#nav-items');    // nav items.
-navitems.appendChild(section3Nav);
+var section3Nav = document.createElement('li');         // li 3
+section3Nav.setAttribute('id','#nav-item3') ;
+section3Nav.textContent =   "Sect 3";
 
 console.log(section3Nav);
+
+
+// creating a new section4 in the nav bar. 
+var section4Nav = document.createElement('li');         // li 4
+section4Nav.setAttribute('id','#nav-item4') ;
+section4Nav.textContent =   "Sect 4";
+
+console.log(section4Nav);
+
+
+// appending nav items.
+var navitems = document.querySelector('#nav-items');    // nav items.
+navitems.appendChild(section3Nav);
+navitems.appendChild(section4Nav);
+
 
 
 
@@ -41,51 +50,22 @@ var section3hr = document.createElement('hr');          // hr
 var section3p = document.createElement('p');            // p
 section3p.textContent = lorem1 + lorem1 + lorem1 + lorem1 + lorem1 + lorem1; 
 
-
 var section3 = document.createElement('div');           // main div .section
 section3.className= 'section';
-section3.setAttribute('id',"section3");
+section3.setAttribute('id', 'section3');
 
 section3.append(section3header, section3hr, section3p);
 
 console.log(section3);
 
 
-// appending section 3 to the rest of sections. 
-var sections = document.querySelector('#sections');     // main div #sections
-sections.appendChild(section3);
-
-
-
-
-
-
-// creating a new section4 in the nav bar. 
-var section4NavLink = document.createElement('a');      // a
-// section4NavLink.setAttribute('href', "#section4");
-section4NavLink.textContent = "Sect 4";
-
-var section4Nav = document.createElement('li');         // li
-section4Nav.className = "#nav-item4";
-section4Nav.appendChild(section4NavLink);
-
-// var navitems = document.querySelector('#nav-items');    // nav items.
-navitems.appendChild(section4Nav);
-
-console.log(section4Nav);
-
-
-
 
 //  creating a new section4 in the main page. 
 var section4header = document.createElement('h1');      // h1
 section4header.textContent = "Section 4:"
-
 var section4hr = document.createElement('hr');          // hr
-
 var section4p = document.createElement('p');            // p
 section4p.textContent = lorem1 + lorem1 + lorem1 + lorem1 + lorem1 + lorem1 + lorem1 + lorem1 + lorem1 + lorem1; 
-
 
 var section4 = document.createElement('div');           // main div .section
 section4.className= 'section';
@@ -93,14 +73,17 @@ section4.setAttribute('id',"section4");
 
 section4.append(section4header, section4hr, section4p);
 
+console.log(section4);
 
 
-
-
-
+// appending section 3 to the rest of sections. 
+var sections = document.querySelector('#sections');     // main div #sections
+sections.appendChild(section3);
 sections.appendChild(section4);
 
-console.log(section4);
+
+
+
 
 // creating an element for the rest of nav sections. 
 var section1Nav = document.querySelector('#nav-item1');
@@ -117,47 +100,52 @@ var sec3dim = section3.getBoundingClientRect();
 var sec4dim = section4.getBoundingClientRect();
 
 
+console.log(section1Nav);
+console.log(section2Nav);
+console.log(section3Nav);
+console.log(section4Nav);
+
 section1Nav.addEventListener('click', () => {
-    window.scrollTo({sec1dim,
-                     behavior: 'smooth'});
+    // location.href = "#section1";
+    section1.scrollIntoView();
 })
 section2Nav.addEventListener('click', () => {
-    window.scrollTo({sec2dim,
-                     behavior: 'smooth'});
+    // location.href = "#section2";
+    section2.scrollIntoView();
 })
 section3Nav.addEventListener('click', () => {
-    window.scrollTo({sec3dim,
-                     behavior: 'smooth'});
+    // location.href = "#section3";
+    section3.scrollIntoView();
 })
 section4Nav.addEventListener('click', () => {
-    window.scrollTo({sec4dim,
-                     behavior: 'smooth'});
+    // location.href = "#section4";
+    section4.scrollIntoView();
 })
 
 
 document.addEventListener('scroll', () => {
-    if (window.scrollY < sec4dim.bottom && window.scrollY > sec4dim.top)
+    if (window.scrollY > sec4dim.top)
     {
         section1Nav.style.backgroundColor = headerBackgroundColor;
         section2Nav.style.backgroundColor = headerBackgroundColor;
         section3Nav.style.backgroundColor = headerBackgroundColor;
         section4Nav.style.backgroundColor = highlightNavItem;
     }
-    else if (window.scrollY < sec3dim.bottom && window.scrollY > sec3dim.top)
+    else if (window.scrollY > sec3dim.top)
     {
         section1Nav.style.backgroundColor = headerBackgroundColor;
         section2Nav.style.backgroundColor = headerBackgroundColor;
         section3Nav.style.backgroundColor = highlightNavItem;
         section4Nav.style.backgroundColor = headerBackgroundColor;
     }
-    else if (window.scrollY < sec2dim.bottom && window.scrollY > sec2dim.top)
+    else if (window.scrollY > sec2dim.top)
     {
         section1Nav.style.backgroundColor = headerBackgroundColor;
         section2Nav.style.backgroundColor = highlightNavItem;
         section3Nav.style.backgroundColor = headerBackgroundColor;
         section4Nav.style.backgroundColor = headerBackgroundColor;
     }
-    else if (window.scrollY < sec1dim.bottom && window.scrollY > sec1dim.top)
+    else if (window.scrollY > sec1dim.top)
     {
         section1Nav.style.backgroundColor = highlightNavItem;
         section2Nav.style.backgroundColor = headerBackgroundColor;
